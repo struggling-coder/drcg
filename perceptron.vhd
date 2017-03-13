3ea8f1647c9bed76ca9d9cf2c5cf9b1499d4ffcb
Aditya Malusare <aditya.malusare@gmail.com>
2017-03-13 12:29:04 +0530

    Array introduction completed
    ...and it doesn't fucking work. Back to the dumb code.

--- perceptron.vhd
+++ perceptron.vhd
@@ -34,50 +34,50 @@ basic: process(input, mode, tr_out) is
 begin
 		tempsum:= 0;
 		if mode = '0' then
-			tempsum := tempsum + weights(0);
 		
-			for i in 1 to 7 loop
-				if input(i) = '1' then
-					tempsum := tempsum + weights(i);
+			tempsum := tempsum + weights(0);
+			for iter1 in 1 to 7 loop
+				if input(iter1) = '1' then
+					tempsum := tempsum + weights(iter1);
 				end if;
 			end loop;
 			
 			if tempsum > 0 then
-				output <= '1'  ;
+				output <= '1';
 			else 
-				output <= '0'  ;
+				output <= '0';
 			end if;
 			
 		elsif mode = '1' then 
 		
 			tempsum := tempsum + weights(0);
-			for i in 1 to 7 loop
-				if input(i) = '1' then
-						tempsum := tempsum + weights(i);
+			for iter2 in 1 to 7 loop
+				if input(iter2) = '1' then
+						tempsum := tempsum + weights(iter2);
 				end if;
 			end loop;
 			
 			if tempsum > 0 then
 				t_out <= '1';
-			elsif tempsum < 0 then	
+			else 	
 				t_out <= '0';
 			end if;
 
 		if t_out = '0' and tr_out = '1' then
 
 			weights(0) := weights(0) + 1;
-			for i in 1 to 7 loop
-				if input(i) = '1' then
-					weights(i) := weights(i) + 1;
+			for iter3 in 1 to 7 loop
+				if input(iter3) = '1' then
+					weights(iter3) := weights(iter3) + 1;
 				end if;
 			end loop;
 			
 		elsif t_out = '1' and tr_out = '0' then
 			
 			weights(0) := weights(0) - 1;
-			for i in 1 to 7 loop
-				if input(i) = '1' then
-					weights(i) := weights(i) - 1;
+			for iter4 in 1 to 7 loop
+				if input(iter4) = '1' then
+					weights(iter4) := weights(iter4) - 1;
 				end if;
 			end loop;
 						
@@ -105,7 +105,6 @@ end entity skeleton;
 architecture muscle of skeleton is
 signal out_array: bit_vector(0 to 9);
 signal tr_array: bit_vector(0 to 9);
-signal status0, status1, status2, status3, status4, status5, status6, status7, status8, status9: bit;
 begin
 
 	layer00 : entity work.perceptron(behaviour)
@@ -177,150 +176,91 @@ port (ct: out integer);
 end entity test_perceptron;
 
 architecture tb of test_perceptron is
-signal in1, in2, in3, in4, in5, in6, in7, tr_out, mode, output, status, done: bit;
+signal input: bit_vector(1 to 7);
+signal tr_out, mode, output: bit;
 signal ctrl: integer range -1 to 20 := -1;
 component perceptron is
-port (in1, in2, in3, in4, in5, in6, in7, tr_out, mode: in bit;
-		output, status: out bit);
+port (input: in bit_vector(1 to 7);
+		tr_out, mode: in bit;
+		output: out bit);
 end component;
 begin
 	-- I have to do this at least once :'(
 	
-	tp: perceptron port map(in1 , in2, in3, in4, in5 , in6, in7, tr_out, mode, output, status);
+	tp: perceptron port map(input, tr_out, mode, output);
 	
 	process(ctrl) is
 	begin
-	
 	ct <= 0;
-	
 	case ctrl is 
 
 	when -1 =>
 	-- digit 0
-	in1 <= '1';
-	in2 <= '1';
-	in3 <= '1';
-	in4 <= '0';
-	in5 <= '1';
-	in6 <= '1';
-	in7 <= '1';
+	input <= "1110111";
 	mode <= '1';
 	tr_out <= '0';
 	ctrl <= ctrl + 1 after 1 ns;
 	
 	when 0 =>
 	-- digit 1
-	in1 <= '0';
-	in2 <= '0';
-	in3 <= '1';
-	in4 <= '0';
-	in5 <= '0';
-	in6 <= '1';
-	in7 <= '0';
+	input <= "0010010";
 	mode <= '1';
 	tr_out <= '0';
 	ctrl <= ctrl + 1 after 1 ns;
 
 	when 1 =>
 	-- digit 2
-	in1 <= '1';
-	in2 <= '0';
-	in3 <= '1';
-	in4 <= '1';
-	in5 <= '1';
-	in6 <= '0';
-	in7 <= '1';
+	input <= "1011101";
 	mode <= '1';
 	tr_out <= '0';
 	ctrl <= ctrl + 1 after 1 ns;
 
 	when 2 =>
 	-- digit 3
-	in1 <= '1';
-	in2 <= '0';
-	in3 <= '1';
-	in4 <= '1';
-	in5 <= '0';
-	in6 <= '1';
-	in7 <= '1';
+	
+	input <= "1011011";
 	mode <= '1';
 	tr_out <= '0';
 	ctrl <= ctrl + 1 after 1 ns;
 
 	when 3 =>
 	-- digit 4
-	in1 <= '0';
-	in2 <= '1';
-	in3 <= '1';
-	in4 <= '1';
-	in5 <= '0';
-	in6 <= '1';
-	in7 <= '0';
+	input <= "0111010";
 	mode <= '1';
 	tr_out <= '0';
 	ctrl <= ctrl + 1 after 1 ns;
 
 	when 4 =>
 	-- digit 5
-	in1 <= '1';
-	in2 <= '0';
-	in3 <= '1';
-	in4 <= '1';
-	in5 <= '1';
-	in6 <= '0';
-	in7 <= '1';
+	input <= "1101011";
 	mode <= '1';
 	tr_out <= '0';
 	ctrl <= ctrl + 1 after 1 ns;
 
 	when 5 =>
 	-- digit 6
-	in1 <= '1';
-	in2 <= '1';
-	in3 <= '0';
-	in4 <= '1';
-	in5 <= '1';
-	in6 <= '1';
-	in7 <= '1';
+	input <= "1101111";
 	mode <= '1';
 	tr_out <= '0';
 	ctrl <= ctrl + 1 after 1 ns;
 
 	when 6 =>
 	-- digit 7
-	in1 <= '1';
-	in2 <= '0';
-	in3 <= '1';
-	in4 <= '0';
-	in5 <= '0';
-	in6 <= '1';
-	in7 <= '0';
+	input <= "1010010";
 	mode <= '1';
 	tr_out <= '0';
 	ctrl <= ctrl + 1 after 1 ns;
 
 	when 7 =>
 	-- digit 8
-	in1 <= '1';
-	in2 <= '1';
-	in3 <= '1';
-	in4 <= '1';
-	in5 <= '1';
-	in6 <= '1';
-	in7 <= '1';
+	input <= "1111111";
 	mode <= '1';
 	tr_out <= '1';
 	ctrl <= ctrl + 1 after 1 ns;
 	
 	when 8 =>
 	-- digit 9
-	in1 <= '1';
-	in2 <= '1';
-	in3 <= '1';
-	in4 <= '1';
-	in5 <= '0';
-	in6 <= '1';
-	in7 <= '1';
+	input <= "1111011";
 	mode <= '1';
 	tr_out <= '0';
 	ctrl <= ctrl + 1 after 1 ns;
@@ -328,28 +268,13 @@ begin
 	
 	when 9 =>
 	mode <= '0';
-	in1 <= '1';
-	in2 <= '1';
-	in3 <= '1';
-	in4 <= '0';
-	in5 <= '1';
-	in6 <= '1';
-	in7 <= '1';
+	input <= "1110111";
 	ctrl <= ctrl + 1 after 1 ns;
 	
 	when 10 =>
-
-	
 	-- digit 1
-	in1 <= '0';
-	in2 <= '0';
-	in3 <= '1';
-	in4 <= '0';
-	in5 <= '0';
-	in6 <= '1';
-	in7 <= '0';
+	input <= "0010010";
 	mode <= '0';
-
 	if output = '1' then 
 		ctrl <= -1 after 1 ns;
 	else 
@@ -358,13 +283,7 @@ begin
 
 	when 11 =>
 	-- digit 2
-	in1 <= '1';
-	in2 <= '0';
-	in3 <= '1';
-	in4 <= '1';
-	in5 <= '1';
-	in6 <= '0';
-	in7 <= '1';
+	input <= "1011101";
 	mode <= '0';
 	if output = '1' then 
 		ctrl <= 0 after 1 ns;
@@ -374,13 +293,7 @@ begin
 
 	when 12 =>
 	-- digit 3
-	in1 <= '1';
-	in2 <= '0';
-	in3 <= '1';
-	in4 <= '1';
-	in5 <= '0';
-	in6 <= '1';
-	in7 <= '1';
+	input <= "1011011";
 	mode <= '0';
 	if output = '1' then 
 		ctrl <= 1 after 1 ns;
@@ -388,16 +301,9 @@ begin
 		ctrl <= ctrl + 1 after 1 ns;
 	end if;
 
-
 	when 13 =>
 	-- digit 4
-	in1 <= '0';
-	in2 <= '1';
-	in3 <= '1';
-	in4 <= '1';
-	in5 <= '0';
-	in6 <= '1';
-	in7 <= '0';
+	input <= "0111010";
 	mode <= '0';
 	if output = '1' then 
 		ctrl <= 2 after 1 ns;
@@ -405,16 +311,9 @@ begin
 		ctrl <= ctrl + 1 after 1 ns;
 	end if;
 
-
 	when 14 =>
 	-- digit 5
-	in1 <= '1';
-	in2 <= '0';
-	in3 <= '1';
-	in4 <= '1';
-	in5 <= '1';
-	in6 <= '0';
-	in7 <= '1';
+	input <= "1101011";
 	mode <= '0';
 	if output = '1' then 
 		ctrl <= 3 after 1 ns;
@@ -423,14 +322,8 @@ begin
 	end if;
 
 	when 15 =>
-	-- digit 6
-	in1 <= '1';
-	in2 <= '1';
-	in3 <= '0';
-	in4 <= '1';
-	in5 <= '1';
-	in6 <= '1';
-	in7 <= '1';
+	-- digit 6	
+	input <= "1101111";
 	mode <= '0';
 	if output = '1' then 
 		ctrl <= 4 after 1 ns;
@@ -438,16 +331,9 @@ begin
 		ctrl <= ctrl + 1 after 1 ns;
 	end if;
 
-
 	when 16 =>
 	-- digit 7
-	in1 <= '1';
-	in2 <= '0';
-	in3 <= '1';
-	in4 <= '0';
-	in5 <= '0';
-	in6 <= '1';
-	in7 <= '0';
+	input <= "1010010";
 	mode <= '0';
 	if output = '1' then 
 		ctrl <= 5 after 1 ns;
@@ -455,33 +341,19 @@ begin
 		ctrl <= ctrl + 1 after 1 ns;
 	end if;
 
-
 	when 17 =>
 	-- digit 8
-	in1 <= '1';
-	in2 <= '1';
-	in3 <= '1';
-	in4 <= '1';
-	in5 <= '1';
-	in6 <= '1';
-	in7 <= '1';
+	input <= "1111111";
 	mode <= '0';
 	if output = '1' then 
 		ctrl <= 6 after 1 ns;
 	else 
 		ctrl <= ctrl + 1 after 1 ns;
 	end if;
-
 	
 	when 18 =>
 	-- digit 9
-	in1 <= '1';
-	in2 <= '1';
-	in3 <= '1';
-	in4 <= '1';
-	in5 <= '0';
-	in6 <= '1';
-	in7 <= '1';
+	input <= "1111011";
 	mode <= '0';
 	if output = '0' then 
 		ctrl <= 7 after 1 ns;
@@ -499,6 +371,7 @@ begin
 	when 20 =>
 	-- test phase reached
 	ct <= 1;
+	input <= "1111111";
 	
 	end case;
 	end process;
