4f4639489e7f796df07c5f7374d4204b95583f4a
Aditya Malusare <aditya.malusare@gmail.com>
2017-03-22 20:25:09 +0530

    Fix the FSM generator

--- fsmgen.py
+++ fsmgen.py
@@ -15,48 +15,66 @@ digits=[
 "1111011"  #9
 ]
 
-print "signal out0, out1, out2, out3, out4, out5, out6, out7, out8, out9: bit;\nsignal tr_out0, tr_out1, tr_out2, tr_out3, tr_out4, tr_out5, tr_out6, tr_out7, tr_out8, tr_out9: bit;"
+print """
+entity final is
+port (dpin1, dpin2, dpin3, dpin4, dpin5, dpin6, dpin7: in bit)
+end entity final;
+"""
+
+print """architecture assembly of final is\n"""
+print "signal out0, out1, out2, out3, out4, out5, out6, out7, out8, out9: bit;"
+print "signal tr_out0, tr_out1, tr_out2, tr_out3, tr_out4, tr_out5, tr_out6, tr_out7, tr_out8, tr_out9: bit;"
 print "type inputs is array (0 to 9) of integer;"
 print "signal inp1, inp2, inp3, inp4, inp5, inp6, inp7: inputs;"
+print "signal data1, data2, data3, data4, data5, data6, data7: inputs;\n"
 
 print "type states is ("
-for j in range(0, 10): print ("capture"+str(j)+",")
+print "".join(["capture"+str(j)+"," for j in range(0, 10)])
 for digit in range(0, 10): print "".join(["pcp_trn"+str(digit)+str(k)+", " for k in range(0, 10)])
 for digit in range(0, 10): print "".join(["pcp_tst"+str(digit)+str(k)+", " for k in range(0, 11)])
+print "ready0, ready1, ready2);"
 
-print "\n\nprocess(ctrl) is\nbegin\ncase ctrl is\n"
+print "\nbegin"
 
-for j in range(0, 10):
-	print "when capture"+str(j)+" =>"
+print "\n\tprocess(ctrl) is\n\tbegin\n\tcase ctrl is\n"
 
+for digit in range(0, 10):
+	print "when capture"+str(digit)+" =>"
+	for d in range(1, 8): print "\t\tdata"+str(d)+"("+str(digit)+") <= '"+digits[digit][d-1]+"';"
+		
 for digit in range(0, 10):
 	
 	heuristic = [0, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 	for z in range(0, len(heuristic)):
 		h = heuristic.pop(0)
-		print "when pcp_trn"+str(digit)+str(h)+" =>"
-		print "\t--TRAIN digit "+ str(h)
-		print "\tmode"+str(digit)+" <= '0';"
-		for d in range(1, 8): print "\tinp"+str(d)+"("+str(digit)+") <= '"+digits[h][d-1]+"';"
-		if h is digit: print "\ttr_out"+str(digit)+" <= '1';"
-		else: print "\ttr_out"+str(digit)+" <= '0';"
-		if len(heuristic) > 0: print "\tctrl <= pcp_trn"+str(digit)+str(heuristic[0])+"' after 1 ns;\n"
-		else: print "\tctrl <= pcp_tst"+str(digit)+"0 after 1 ns;\n"
+		print "\twhen pcp_trn"+str(digit)+str(h)+" =>"
+		print "\t\t--TRAIN digit "+ str(h)
+		print "\t\tmode"+str(digit)+" <= '0';"
+		for d in range(1, 8): print "\t\tinp"+str(d)+"("+str(digit)+") <= " + "data"+str(d)+"("+str(h)+");"
+		if h is digit: print "\t\ttr_out"+str(digit)+" <= '1';"
+		else: print "\t\ttr_out"+str(digit)+" <= '0';"
+		if len(heuristic) > 0: print "\t\tctrl <= pcp_trn"+str(digit)+str(heuristic[0])+"' after 1 ns;\n"
+		else: print "\t\tctrl <= pcp_tst"+str(digit)+"0 after 1 ns;\n"
 	
-	print "when pcp_tst"+str(digit)+"0 =>"
-	for d in range(1, 8): print "\tinp"+str(d)+"("+str(digit)+") <= '"+digits[0][d-1]+"';"		
-	print "\tmode <= '0';\n\tctrl <= pcp_tst"+str(digit)+"1 after 1 ns;"
+	print "\twhen pcp_tst"+str(digit)+"0 =>"
+	for d in range(1, 8): print "\t\tinp"+str(d)+"("+str(digit)+") <= " + "data"+str(d)+"("+str(0)+");"	
+	print "\t\tmode <= '0';\n\t\tctrl <= pcp_tst"+str(digit)+"1 after 1 ns;\n"
 
 	for z in range(1, 10):
-		print "when pcp_tst"+str(digit)+str(z)+" =>"
-		print "\t--TEST digit "+ str(digit)
-		for d in range(1, 8): print "\tinp"+str(d)+"("+str(digit)+") <= '"+digits[z][d-1]+"';"
-		print "\tmode <= '0';"
-		if z is digit + 1: print "\tif out"+str(digit)+" = '0' then"
-		else: print "\tif out"+str(digit)+" = '1' then"
-		print "\t\tctrl <= pcp_trn"+str(digit)+str(z-1)+"after 1 ns;"
-		print "\telse"
-		if z < 10: print "\t\tctrl <= pcp_tst"+str(digit)+str(z + 1)+"after 1 ns";
-		else: print "\t\tctrl <= pcp_trn"+str(digit+1)+"0 after 1 ns;"
+		print "\twhen pcp_tst"+str(digit)+str(z)+" =>"
+		print "\t\t--TEST digit "+ str(digit)
+		for d in range(1, 8): print "\t\tinp"+str(d)+"("+str(digit)+") <= " + "data"+str(d)+"("+str(z)+");"
+		print "\t\tmode <= '0';"
+		if z is digit + 1: print "\t\tif out"+str(digit)+" = '0' then"
+		else: print "\t\tif out"+str(digit)+" = '1' then"
+		print "\t\t\tctrl <= pcp_trn"+str(digit)+str(z-1)+" after 1 ns;"
+		print "\t\telse"
+		print "\t\t\tctrl <= pcp_tst"+str(digit)+str(z + 1)+" after 1 ns;\n"
+	print "\twhen pcp_tst"+str(digit)+"10 => \n\t\t--TEST digit "+str(digit)
+	if 10 is digit + 1: print "\t\tif out"+str(digit)+" = '0' then"
+	else: print "\t\tif out"+str(digit)+" = '1' then"
+	print "\t\t\tctrl <= pcp_trn"+str(digit)+str(9)+" after 1 ns;"
+	print "\t\telse"
+	print "\t\t\tctrl <= pcp_tst"+str(digit+1)+str(0)+" after 1 ns;\n"
 
 print "\tend case;\nend process;"
