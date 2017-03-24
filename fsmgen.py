3488890727f5ebf1941d5251da319fb9d64aa89f
Aditya Malusare <aditya.malusare@gmail.com>
2017-03-24 17:29:35 +0530

    Working neural net code
    Although a LOT of optimization can be done, I'm not going to

--- fsmgen.py
+++ fsmgen.py
@@ -23,7 +23,6 @@ end entity layer;
 """
 
 print """architecture assembly of layer is\n"""
-print "signal ctrl: states:= capture0;"
 print "shared variable failsafe: integer:= 0;"
 print "signal mode0, mode1, mode2, mode3, mode4, mode5, mode6, mode7, mode8, mode9: bit;"
 print "signal tr_out0, tr_out1, tr_out2, tr_out3, tr_out4, tr_out5, tr_out6, tr_out7, tr_out8, tr_out9: bit;"
@@ -34,8 +33,11 @@ print "signal data1, data2, data3, data4, data5, data6, data7: inputs;\n"
 print "type states is ("
 print "".join(["capture"+str(j)+", " for j in range(0, 10)])
 for digit in range(0, 10): print "".join(["pcp_trn"+str(digit)+str(k)+", " for k in range(0, 10)])
-for digit in range(0, 10): print "".join(["pcp_tst"+str(digit)+str(k)+", " for k in range(0, 11)])
-print "pcp_trn100, standby);"
+for digit in range(0, 10): print "".join(["pcp_tst"+str(digit)+str(k)+", " for k in range(0, 10)])
+for digit in range(0, 10): print "".join(["pcp_tsth"+str(digit)+str(k)+", " for k in range(0, 10)])
+for digit in range(0, 10): print "pcp_standby"+str(digit)+", "
+print "pcp_trn100);"
+print "signal ctrl: states:= capture0;"
 
 print """
 component perceptron is
@@ -62,7 +64,6 @@ for digit in range(0, 10):
 	for h in range(0, 10):
 		print "\twhen pcp_trn"+str(digit)+str(h)+" =>"
 		print "\t\t--TRAIN digit "+ str(h)
-		if digit is 5: print "\t\tfailsafe := failsafe + 1;"
 		print "\t\tmode"+str(digit)+" <= '1';"
 		for d in range(1, 8): print "\t\tinp"+str(d)+"("+str(digit)+") <= " + "data"+str(d)+"("+str(h)+");"
 		if h is digit: print "\t\ttr_out"+str(digit)+" <= '1';"
@@ -70,32 +71,39 @@ for digit in range(0, 10):
 		if h < 9: print "\t\tctrl <= pcp_trn"+str(digit)+str(h+1)+" after 1 ns;\n"
 		else: print "\t\tctrl <= pcp_tst"+str(digit)+"0 after 1 ns;\n"
 		
-	print "\twhen pcp_tst"+str(digit)+"0 =>"
-	for d in range(1, 8): print "\t\tinp"+str(d)+"("+str(digit)+") <= " + "data"+str(d)+"("+str(0)+");"	
-	print "\t\tmode"+str(digit)+" <= '0';\n\t\tctrl <= pcp_tst"+str(digit)+"1 after 1 ns;\n"
+#	print "\twhen pcp_tst"+str(digit)+"0 =>"
+#	for d in range(1, 8): print "\t\tinp"+str(d)+"("+str(digit)+") <= " + "data"+str(d)+"("+str(0)+");"	#
+#	print "\t\tmode"+str(digit)+" <= '0';\n\t\tctrl <= pcp_tsth"+str(digit)+"0 after 1 ns;\n"
 
-	for z in range(1, 10):
+	for z in range(0, 10):
 		print "\twhen pcp_tst"+str(digit)+str(z)+" =>"
 		print "\t\t--TEST digit "+ str(z)
-		if digit is 5:
-			print "\t\tif failsafe > 50000 then"
-			print "\t\t\tctrl <= pcp_trn60;"
-			print "\t\tend if;"
 		for d in range(1, 8): print "\t\tinp"+str(d)+"("+str(digit)+") <= " + "data"+str(d)+"("+str(z)+");"
 		print "\t\tmode"+str(digit)+" <= '0';"
-		if z is digit + 1: print "\t\tif out"+str(digit)+" = '0' then"
-		else: print "\t\tif out"+str(digit)+" = '1' then"
-		print "\t\t\tctrl <= pcp_trn"+str(digit)+str(z-1)+" after 1 ns;"
-		print "\t\telse"
-		print "\t\t\tctrl <= pcp_tst"+str(digit)+str(z + 1)+" after 1 ns;"
-		print "\t\tend if;\n"
-	print "\twhen pcp_tst"+str(digit)+"10 => \n\t\t--TEST digit "+str(digit)
-	if 10 is digit + 1: print "\t\tif out"+str(digit)+" = '0' then"
-	else: print "\t\tif out"+str(digit)+" = '1' then"
-	print "\t\t\tctrl <= pcp_trn"+str(digit)+str(9)+" after 1 ns;"
-	print "\t\telse"
-	print "\t\t\tctrl <= pcp_trn"+str(digit+1)+str(0)+" after 1 ns;"
-	print "\t\tend if;\n"
+		print "\t\tctrl <= pcp_tsth"+str(digit)+str(z)+" after 1 ns;\n"	
+
+		if z < 9:
+			print "\twhen pcp_tsth"+str(digit)+str(z)+" =>"
+			if z is digit: print "\t\tif out"+str(digit)+" = '0' then"
+			else: print "\t\tif out"+str(digit)+" = '1' then"
+			print "\t\t\tctrl <= pcp_standby"+str(digit)+" after 1 ns;"
+			print "\t\telse"
+			print "\t\t\tctrl <= pcp_tst"+str(digit)+str(z + 1)+" after 1 ns;"
+			print "\t\tend if;\n"
+
+		else:
+			print "\twhen pcp_tsth"+str(digit)+"9 => \n\t\t--TEST digit "+str(digit)
+			if 9 is digit: print "\t\tif out"+str(digit)+" = '0' then"
+			else: print "\t\tif out"+str(digit)+" = '1' then"
+			print "\t\t\tctrl <= pcp_standby"+str(digit)+" after 1 ns;"
+			print "\t\telse"
+			print "\t\t\tctrl <= pcp_trn"+str(digit+1)+str(0)+" after 1 ns;"
+			print "\t\tend if;\n"
+
+	print "\twhen pcp_standby"+str(digit)+" => "
+	print "\t\tmode"+str(digit)+" <= '0';"
+	print "\t\tctrl <= pcp_trn"+str(digit)+"0 after 1 ns;\n"
+
 print "when pcp_trn100 => \n --WHATEVER"
 
 print "\tend case;\nend process;\nend architecture assembly;"
--- perceptron.vhd
+++ perceptron.vhd
@@ -14,10 +14,6 @@
 --  |---- 7 ----|
 --  
 --  For the perceptron, mode 0 is test and mode 1 is train
---
---  WARNING:
---  Huge Flying Spaghetti Monster ahead!!
---
 
 entity perceptron is
 port (in1, in2, in3, in4, in5, in6, in7, tr_out, mode: in bit;
@@ -28,16 +24,15 @@ end entity perceptron;
 architecture behaviour of perceptron is
 type arrayw is array(0 to 7) of integer;
 shared variable weights: arrayw:= (others => 0);
-shared variable tempsum: integer:= 0;
-signal t_out: bit;
+shared variable tempsum: integer := 0;
+shared variable t_out: bit;
+shared variable placeholder, panic: integer;
 
 begin
 basic: process(in1, in2, in3, in4, in5, in6, in7, mode, tr_out) is
-variable placeholder: integer;
 begin
-		placeholder:= 0;
 		tempsum:= 0;
-		
+		placeholder := 0;
 		if mode = '0' then
 			tempsum := tempsum + weights(0);
 			if in1 = '1' then
@@ -63,13 +58,13 @@ begin
 			end if;
 			
 			if tempsum > 0 then
-				output <= '1'  ;
+				output <= '1';
 			else 
-				output <= '0'  ;
+				output <= '0';
 			end if;
 			
 		elsif mode = '1' then 
-		output <= '0';
+		
 			tempsum := tempsum + weights(0);
 			placeholder := placeholder + 1;
 			if in1 = '1' then
@@ -101,10 +96,10 @@ begin
 				tempsum := tempsum + weights(7);
 			end if;
 				
-		if tempsum > 0 and placeholder = 8 then
-			t_out <= '1';
-		elsif tempsum <= 0 and placeholder = 8 then	
-			t_out <= '0';
+		if tempsum > 0 then--and placeholder = 8 then
+			t_out := '1';
+		else --and placeholder = 8 then	
+			t_out := '0';
 		end if;
 
 		if t_out = '0' and tr_out = '1' then
@@ -160,48 +155,4 @@ begin
 	end if;
 end process basic;
 
---wchange: process(in1, in2, in3, in4, in5, in6, in7, t_out, tr_out) is
---end process wchange;
-
 end architecture behaviour;
-
-entity skeleton is
-port (inp1, inp2, inp3, inp4, inp5, inp6, inp7, mode: in bit;
-		digit: in integer;	
-		d_output: out integer
-		);
-end entity skeleton;
-
-architecture muscle of skeleton is
-signal out0, out1, out2, out3, out4, out5, out6, out7, out8, out9: bit;
-signal tr_out0, tr_out1, tr_out2, tr_out3, tr_out4, tr_out5, tr_out6, tr_out7, tr_out8, tr_out9: bit;
-
-begin
-	layer00 : entity work.perceptron(behaviour)
-		port map (inp1, inp2, inp3, inp4, inp5, inp6, inp7, tr_out0, mode, out0);
-	layer01 : entity work.perceptron(behaviour)
-		port map (inp1, inp2, inp3, inp4, inp5, inp6, inp7, tr_out1, mode, out1);
-	layer02 : entity work.perceptron(behaviour)
-		port map (inp1, inp2, inp3, inp4, inp5, inp6, inp7, tr_out2, mode, out2);
-	layer03 : entity work.perceptron(behaviour)
-		port map (inp1, inp2, inp3, inp4, inp5, inp6, inp7, tr_out3, mode, out3);
-	layer04 : entity work.perceptron(behaviour)
-		port map (inp1, inp2, inp3, inp4, inp5, inp6, inp7, tr_out4, mode, out4);
-	layer05 : entity work.perceptron(behaviour)
-		port map (inp1, inp2, inp3, inp4, inp5, inp6, inp7, tr_out5, mode, out5);
-	layer06 : entity work.perceptron(behaviour)
-		port map (inp1, inp2, inp3, inp4, inp5, inp6, inp7, tr_out6, mode, out6);
-	layer07 : entity work.perceptron(behaviour)
-		port map (inp1, inp2, inp3, inp4, inp5, inp6, inp7, tr_out7, mode, out7);
-	layer08 : entity work.perceptron(behaviour)
-		port map (inp1, inp2, inp3, inp4, inp5, inp6, inp7, tr_out8, mode, out8);
-	layer09 : entity work.perceptron(behaviour)
-		port map (inp1, inp2, inp3, inp4, inp5, inp6, inp7, tr_out9, mode, out9);
-	
-	
-
-end architecture muscle;
-
-entity skin is
-end entity skin;
-
